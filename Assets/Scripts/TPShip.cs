6b5dda8f6149738dbbf28783ead4d33a1766aa7c
Valentinlansigu <94831002+Valentinlansigu@users.noreply.github.com>
2025-06-09 14:56:05 +0200

    new tp script in scenes

---
+++ Assets/Scripts/TPShip.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TPShip : MonoBehaviour
+{
+    [Header("XR Rig")]
+    public Transform xrRigCamera;
+    public float canvasDistance = 3f;
+
+    [Header("UI Canvas")]
+    public GameObject uiCanvas;
+
+    [Header("XR Movement")]
+    public ActionBasedContinuousMoveProvider moveProvider;
+    public ActionBasedContinuousTurnProvider turnProvider;
+
+    private float toggleCooldown = 0.5f;
+    private float lastToggleTime = 0f;
+    private bool isUIVisible = false;
+
+    void Start()
+    {
+        if (uiCanvas != null)
+            uiCanvas.SetActive(false);
+    }
+
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+
+        if (device.TryGetFeatureValue(CommonUsages.secondaryButton, out bool isSecondaryPressed))
+        {
+            if (isSecondaryPressed && Time.time - lastToggleTime > toggleCooldown)
+            {
+                ToggleUI();
+                lastToggleTime = Time.time;
+            }
+        }
+    }
+
+    void ToggleUI()
+    {
+        isUIVisible = !isUIVisible;
+
+        if (uiCanvas == null)
+            return;
+
+        uiCanvas.SetActive(isUIVisible);
+
+        if (isUIVisible)
+        {
+            PositionCanvasInFront();
+            DisableMovement();
+        }
+        else
+        {
+            EnableMovement();
+        }
+    }
+
+    void PositionCanvasInFront()
+    {
+        Vector3 forward = xrRigCamera.forward;
+        forward.y = 0;
+        forward.Normalize();
+
+        uiCanvas.transform.position = xrRigCamera.position + forward * canvasDistance;
+        uiCanvas.transform.LookAt(xrRigCamera.position);
+        uiCanvas.transform.Rotate(0, 180f, 0);
+    }
+
+    void DisableMovement()
+    {
+        if (moveProvider != null) moveProvider.enabled = false;
+        if (turnProvider != null) turnProvider.enabled = false;
+    }
+
+    void EnableMovement()
+    {
+        if (moveProvider != null) moveProvider.enabled = true;
+        if (turnProvider != null) turnProvider.enabled = true;
+    }
+}
